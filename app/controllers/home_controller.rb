4da7cfd07851065c28f8dc3d306d1f783254e6e1
Stefan Trauth <mail@stefantrauth.de>
2013-03-08 14:56:46 +0100

    add docu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,15 +5,18 @@ require "uri"
 
 class HomeController < ApplicationController
   def index
+	# read the program for today via GET request as JSON from the Airtime radio API
 	uri = URI.parse("http://programm.reliveradio.de/api/today-info")
-	 
 	http = Net::HTTP.new(uri.host, uri.port)
 	request = Net::HTTP::Get.new(uri.request_uri)
-	 
 	response = http.request(request)
-	 
+
+	# check if HTTP request worked
 	if response.code == "200"
+      # parse JSON
 	  result = JSON.parse(response.body)
+	  # save received episodes into episodes object
+	  # the AT means, that we can use this variable also in the corresponding view
 	  @episodes = result
 	else
 	  puts "ERROR: Could not read todays episode list from server."
