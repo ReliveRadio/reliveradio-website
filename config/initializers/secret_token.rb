b989bfe91de043f4068348af1e53c8dc25c06377
Stefan Trauth <mail@stefantrauth.de>
2013-03-08 12:53:23 +0100

    initial rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reliveradio::Application.config.secret_token = '41898914054fc8c394574b19af2105518e53523f6f26606acbff94ce17b7db9d4bdfce78ceee2abdf88d1067e12224e82bec0664f300d890886c4ea935c59a27'
