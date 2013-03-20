6f386786b72e205a793c8d6760a8948f6250f8d2
Stefan Trauth <mail@stefantrauth.de>
2013-03-20 20:53:29 +0100

    fix indention

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,46 +2,46 @@ require "net/http"
 
 class HomeController < ApplicationController
 
-  caches_action :index, :expires_in => 60.seconds, :cache_path => 'index'
+	caches_action :index, :expires_in => 60.seconds, :cache_path => 'index'
 
-  def index
+	def index
 
-	# read the program for today via GET request as JSON from the Airtime radio API
-	uri = URI.parse("http://programm.reliveradio.de/api/today-info")
-	http = Net::HTTP.new(uri.host, uri.port)
-	request = Net::HTTP::Get.new(uri.request_uri)
-	response = http.request(request)
+		# read the program for today via GET request as JSON from the Airtime radio API
+		uri = URI.parse("http://programm.reliveradio.de/api/today-info")
+		http = Net::HTTP.new(uri.host, uri.port)
+		request = Net::HTTP::Get.new(uri.request_uri)
+		response = http.request(request)
 
-	# check if HTTP request worked
-	if response.code == "200"
-		# parse JSON and save received episodes into episodes object
-		@episodes = JSON.parse(response.body)
+		# check if HTTP request worked
+		if response.code == "200"
+			# parse JSON and save received episodes into episodes object
+			@episodes = JSON.parse(response.body)
 
-		# remove all passed podcasts from the episodes array
-		@episodes.delete_if { |episode| Time.parse(episode["ends"])+1.hour < Time.now }
+			# remove all passed podcasts from the episodes array
+			@episodes.delete_if { |episode| Time.parse(episode["ends"])+1.hour < Time.now }
 
-		# add some more metadata to episodes array
-		@episodes.each do |episode|
-			# set all to not being live (first one will be set to live after this iterator)
-			episode["isLive"] = false;
+			# add some more metadata to episodes array
+			@episodes.each do |episode|
+				# set all to not being live (first one will be set to live after this iterator)
+				episode["isLive"] = false;
 
-			# adjust time
-			starts = Time.parse(episode["starts"]) + 1.hour
-			ends = Time.parse(episode["ends"]) + 1.hour
-			episode["starts"] = starts
-			episode["ends"] = ends
-			# calculate the duration of this episode
-			episode["duration"] = ((ends - starts) / 60).round
+				# adjust time
+				starts = Time.parse(episode["starts"]) + 1.hour
+				ends = Time.parse(episode["ends"]) + 1.hour
+				episode["starts"] = starts
+				episode["ends"] = ends
+				# calculate the duration of this episode
+				episode["duration"] = ((ends - starts) / 60).round
 
-			# add database information to this object to easily access that in view
-			episode["db"] = Podcast.where(["artistname = ?", episode['artist_name']]).first
+				# add database information to this object to easily access that in view
+				episode["db"] = Podcast.where(["artistname = ?", episode['artist_name']]).first
+			end
+
+			# first episode in array is live!
+			@episodes.first["isLive"] = true;
+
+		else
+			puts "ERROR: Could not read todays episode list from server."
 		end
-
-		# first episode in array is live!
-		@episodes.first["isLive"] = true;
-
-	else
-		puts "ERROR: Could not read todays episode list from server."
 	end
-  end
 end
