711f2523a215e52fec0f4c4469bb92d25fcdcd01
Stefan Trauth <mail@stefantrauth.de>
2013-04-06 13:11:51 +0200

    move episodes schedule fetcher into home controller helper

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -23,50 +23,12 @@ class HomeController < ApplicationController
 	end
 
 	def index
-
 		# fetch live listeners count
 		@listeners = fetch_listeners
 		# fetch really live podcasts
 		@live_podcasts = fetch_hoersuppe_livepodcasts
-
-		# airtime api returns UTC dates
-
-		#Rails.cache.delete("cacheID")
-		@episodes = Rails.cache.fetch("airtime_schedule", :expires_in => 10.minutes) do
-			# read the program for today via GET request as JSON from the Airtime radio API
-			uri = URI.parse("http://programm.reliveradio.de/api/today-info")
-			http = Net::HTTP.new(uri.host, uri.port)
-			request = Net::HTTP::Get.new(uri.request_uri)
-			response = http.request(request)
-			JSON.parse(response.body)
-		end
-
-		if !@episodes.blank?
-			# remove all passed podcasts from the episodes array
-			@episodes.delete_if { |episode| DateTime.parse(episode["ends"]).utc.in_time_zone("Berlin") < Time.now.utc.in_time_zone("Berlin") }
-			# do not display jingles in the schedule
-			@episodes.delete_if { |episode| episode["artistname"] == "jingle" }
-
-			# add some more metadata to episodes array
-			@episodes.each do |episode|
-				# set all to not being live (first one will be set to live after this iterator)
-				episode["isLive"] = false;
-
-				# adjust time
-				starts = DateTime.parse(episode["starts"]).utc.in_time_zone("Berlin")
-				ends = DateTime.parse(episode["ends"]).utc.in_time_zone("Berlin")
-				episode["starts"] = starts
-				episode["ends"] = ends
-				# calculate the duration of this episode
-				episode["duration"] = ((ends - starts) / 60).round
-
-				# add database information to this object to easily access that in view
-				episode["db"] = Podcast.where(["artistname = ?", episode['artist_name']]).first
-			end
-
-			# first episode in array is live!
-			@episodes.first["isLive"] = true;
-		end
+		# fetch episode schedule
+		@episodes = fetch_episode_schedule
 
 		respond_to do |format|
 			format.html # index.html.erb
