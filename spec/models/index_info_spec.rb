750dc81ecfb5c078b70d5a1a742260dacda917d1
Stefan Trauth <mail@stefantrauth.de>
2013-07-22 23:13:29 +0200

    test index info model

---
+++ spec/models/index_info_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe IndexInfo do
+
+	it "has a valid factory" do
+	 	FactoryGirl.create(:index_info).should be_valid
+	end
+
+	it "is invalid without content" do
+		FactoryGirl.build(:index_info, content: nil).should_not be_valid
+	end
+
+	it "is invalid without a title" do
+	 	FactoryGirl.build(:index_info, title: nil).should_not be_valid
+	end
+
+end
