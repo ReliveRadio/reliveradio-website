9e6ffe68fb035b5d0f626dd2c79e7b8c7ff9519c
Stefan Trauth <mail@stefantrauth.de>
2013-07-22 23:40:37 +0200

    comment these test. user experience with these restionctions very bad

--- spec/models/index_info_spec.rb
+++ spec/models/index_info_spec.rb
@@ -6,12 +6,12 @@ describe IndexInfo do
 	 	FactoryGirl.create(:index_info).should be_valid
 	end
 
-	it "is invalid without content" do
-		FactoryGirl.build(:index_info, content: nil).should_not be_valid
-	end
+	# it "is invalid without content" do
+	# 	FactoryGirl.build(:index_info, content: nil).should_not be_valid
+	# end
 
-	it "is invalid without a title" do
-	 	FactoryGirl.build(:index_info, title: nil).should_not be_valid
-	end
+	# it "is invalid without a title" do
+	#  	FactoryGirl.build(:index_info, title: nil).should_not be_valid
+	# end
 
 end
