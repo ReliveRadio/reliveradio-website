f8434b7eb74f45a85109bed52f5e53f18fde212f
Stefan Trauth <mail@stefantrauth.de>
2013-07-22 23:13:44 +0200

    add new unimplemented tests to podcasts model

--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -6,6 +6,10 @@ describe Podcast do
 	 	FactoryGirl.create(:podcast).should be_valid
 	end
 
+	it "creates a new podcast in DB" do
+		
+	end
+
 	it "is invalid without an artistname" do
 	    FactoryGirl.build(:podcast, artistname: nil).should_not be_valid
 	end
@@ -28,5 +32,17 @@ describe Podcast do
 
 	it "is invalid without a description" do
 	    FactoryGirl.build(:podcast, description: nil).should_not be_valid	  
+	end
+
+	it "finds the correct podcasts via search if it exists" do
+	  
+	end
+
+	it "returns all podcasts when search is empty" do
+	  
+	end
+
+	it "returns an empty list if search does not find any matching podcast" do
+	  
 	end
 end
