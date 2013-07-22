c9f0433855298528a53cd6728a71aecdc777c6a1
Stefan Trauth <mail@stefantrauth.de>
2013-07-22 23:26:32 +0200

    implement all test cases and add context to cluster some test cases

--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -2,47 +2,50 @@ require 'spec_helper'
 
 describe Podcast do
 
-	it "has a valid factory" do
-	 	FactoryGirl.create(:podcast).should be_valid
+	context "default model validation" do  
+		it "has a valid factory" do
+		 	FactoryGirl.create(:podcast).should be_valid
+		end
+
+		it "is invalid without an artistname" do
+		    FactoryGirl.build(:podcast, artistname: nil).should_not be_valid
+		end
+
+		it "is invalid without a name" do
+		    FactoryGirl.build(:podcast, name: nil).should_not be_valid   
+		end
+
+		it "is invalid without a feedurl" do
+		    FactoryGirl.build(:podcast, feedurl: nil).should_not be_valid  
+		end
+
+		it "is invalid without a slugintern" do
+		    FactoryGirl.build(:podcast, slugintern: nil).should_not be_valid  
+		end
+
+		it "is invalid without an URL" do
+		    FactoryGirl.build(:podcast, url: nil).should_not be_valid	  
+		end
+
+		it "is invalid without a description" do
+		    FactoryGirl.build(:podcast, description: nil).should_not be_valid	  
+		end
 	end
 
-	it "creates a new podcast in DB" do
-		
+
+	context "searching in podcast database" do  
+		it "finds the correct podcasts via search if it exists" do
+			p = FactoryGirl.create(:podcast)
+			Podcast.search(p.name).should include(p)
+		end
+
+		it "returns all podcasts when search is empty" do
+			Podcast.search("").should == Podcast.all
+		end
+
+		it "returns an empty list if search does not find any matching podcast" do
+			Podcast.search("adfglhjaergouhadfgjhdfg").should be_empty
+		end
 	end
 
-	it "is invalid without an artistname" do
-	    FactoryGirl.build(:podcast, artistname: nil).should_not be_valid
-	end
-
-	it "is invalid without a name" do
-	    FactoryGirl.build(:podcast, name: nil).should_not be_valid   
-	end
-
-	it "is invalid without a feedurl" do
-	    FactoryGirl.build(:podcast, feedurl: nil).should_not be_valid  
-	end
-
-	it "is invalid without a slugintern" do
-	    FactoryGirl.build(:podcast, slugintern: nil).should_not be_valid  
-	end
-
-	it "is invalid without an URL" do
-	    FactoryGirl.build(:podcast, url: nil).should_not be_valid	  
-	end
-
-	it "is invalid without a description" do
-	    FactoryGirl.build(:podcast, description: nil).should_not be_valid	  
-	end
-
-	it "finds the correct podcasts via search if it exists" do
-	  
-	end
-
-	it "returns all podcasts when search is empty" do
-	  
-	end
-
-	it "returns an empty list if search does not find any matching podcast" do
-	  
-	end
 end
