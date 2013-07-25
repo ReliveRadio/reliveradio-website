a97977502867518cc053ebbdfdd129413b1c2bf5
Stefan Trauth <mail@stefantrauth.de>
2013-07-25 21:39:25 +0200

    some more simple routes test example

--- spec/routing/podcast_routes_spec.rb
+++ spec/routing/podcast_routes_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe PodcastsController do
-	it "should route podcast/new" do
+	it "should route /podcasts/new" do
 	  {get: '/podcasts/new'}.should route_to({controller: 'podcasts', action: 'new'})
+	end
+	it "should route /podcast" do
+	  {get: '/podcasts'}.should route_to({controller: 'podcasts', action: 'index'})
+	end
+	it "should route /podcasts with POST" do
+	  {post: '/podcasts'}.should route_to({controller: 'podcasts', action: 'create'})
 	end
 end
