b890bb5c9944f760ca2621b1d779300e8f8e2c6e
Stefan Trauth <mail@stefantrauth.de>
2013-07-25 20:01:26 +0200

    more tests for podcast controller

--- spec/controllers/podcasts_controller_spec.rb
+++ spec/controllers/podcasts_controller_spec.rb
@@ -26,13 +26,61 @@ describe PodcastsController do
 	end
 
 	describe "GET #show" do
-		it "assigns the requested contact to @contact"
-		it "renders the :show template"
+		it "renders the :show template" do
+			p = FactoryGirl.create(:podcast)
+			get :show, id: p.id
+			response.should render_template(:show)
+		end
+		it "should be successful" do
+			p = FactoryGirl.create(:podcast)
+			get :show, id: p.id
+			response.should be_success
+		end
+		it "assigns the requested id to @podcast" do
+			p = FactoryGirl.create(:podcast)
+			get :show, id: p.id
+			assigns[:podcast].should_not be_nil
+			assigns[:podcast].should be_kind_of(Podcast)
+			assigns[:podcast].should == p
+		end
+		it "should redirect to #index if no podcast can be found for the given id" do
+			get :show, id: "some_id_that_is_not_in_database"
+			response.should be_success
+			response.should redirect_to(podcasts_path)
+		end
 	end
 
 	describe "GET #new" do
-		it "assigns a new Contact to @contact"
-		it "renders the :new template"
+		it "assigns a new Podcast object" do
+			get :new
+			assigns[:podcast].should_not be_nil
+			assigns[:podcast].should be_kind_of(Podcast)
+			assigns[:podcast].should be_new_record
+		end
+		it "renders the :new template" do
+			get :new
+			response.should render_template(:new)
+		end
+		it "should be successful" do
+			get :new
+			response.should be_success
+		end
+	end
+
+	describe "GET #overview" do
+		
+	end
+
+	describe "GET #info" do
+		
+	end
+
+	describe "GET #edit" do
+		
+	end
+
+	describe "POST #import" do
+		
 	end
 
 	describe "POST #create" do
@@ -44,6 +92,22 @@ describe PodcastsController do
 			it "does not save the new contact in the database"
 			it "re-renders the :new template"
 		end
-	end 
+	end
+
+	describe "POST #destroy" do
+		
+	end
+
+	describe "POST #update" do
+		
+	end
+
+	describe "POST #create" do
+		
+	end
+
+	describe "POST #import_from_feed" do
+		
+	end
 
 end
