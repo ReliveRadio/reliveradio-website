d31126eabb86f6190d30be17cc2ef041fbd65a6c
Stefan Trauth <mail@stefantrauth.de>
2013-07-31 12:52:15 +0200

    fix typo in routes spec

--- spec/routing/podcast_routes_spec.rb
+++ spec/routing/podcast_routes_spec.rb
@@ -4,7 +4,7 @@ describe PodcastsController do
 	it "should route /podcasts/new" do
 	  {get: '/podcasts/new'}.should route_to({controller: 'podcasts', action: 'new'})
 	end
-	it "should route /podcast" do
+	it "should route /podcasts" do
 	  {get: '/podcasts'}.should route_to({controller: 'podcasts', action: 'index'})
 	end
 	it "should route /podcasts with POST" do
