58da088d75fa5306d7d1f7caa4a40529444726bb
Stefan Trauth <mail@stefantrauth.de>
2013-08-02 22:10:46 +0200

    add contexts for index spec and add spec for links to streams

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,14 +1,30 @@
 require 'spec_helper'
 
 describe "Index" do
-	it "shows index_info data if display is set to true" do
-		index_info = FactoryGirl.create(:index_info)
-		visit index_url
-		page.should have_content index_info.content
+	context "IndexInfo" do
+		it "shows index_info data if display is set to true" do
+			index_info = FactoryGirl.create(:index_info)
+			visit index_url
+			page.should have_content index_info.content
+		end
+		it "does not show index_info if display is set to false" do
+			index_info = FactoryGirl.create(:index_info, display: false)
+			visit index_url
+			page.should_not have_content index_info.content
+		end
 	end
-	it "does not show index_info if display is set to false" do
-		index_info = FactoryGirl.create(:index_info, display: false)
-		visit index_url
-		page.should_not have_content index_info.content
+	context "Links" do
+		it "should link to mix stream page" do
+			visit index_url
+			page.should have_link("Mix", :href => mix_stream_url)
+		end
+		it "should link to technique stream page" do
+			visit index_url
+			page.should have_link("Technik", :href => technique_stream_url)
+		end
+		it "should link to culture stream page" do
+			visit index_url
+			page.should have_link("Kultur", :href => culture_stream_url)
+		end
 	end
 end
