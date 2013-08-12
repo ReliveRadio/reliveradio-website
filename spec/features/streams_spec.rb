e6bae2b57141991e572ca56b60bb4c400aa21381
Stefan Trauth <mail@stefantrauth.de>
2013-08-12 20:34:06 +0200

    add a test to ensure the playslist creator credit appears on every stream page

--- spec/features/streams_spec.rb
+++ spec/features/streams_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe "Streams" do
+
+	describe "#General" do
+		it "should contain a widget to credit the playlist creator" do
+			visit mix_stream_url
+			page.should have_content "Das Programm auf diesem Kanal wird von"
+		end
+	end
+
+	describe "#Mix" do
+		
+	end
+
+	describe "#Technique" do
+		
+	end
+
+	describe "#Culture" do
+		
+	end
 	
 end
