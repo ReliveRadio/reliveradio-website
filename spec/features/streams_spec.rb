556fcd3567f532280e3ebc3a7e24ef086c9a9766
Stefan Trauth <mail@stefantrauth.de>
2013-08-15 08:55:38 +0200

    test for playlist creator credit on every stream page

--- spec/features/streams_spec.rb
+++ spec/features/streams_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe "Streams" do
 
 	describe "#General" do
+	end
+
+	describe "#Mix" do		
 		it "should contain a widget to credit the playlist creator" do
 			VCR.use_cassette('stream-mix') do
 				visit mix_stream_url
@@ -11,16 +14,22 @@ describe "Streams" do
 		end
 	end
 
-	describe "#Mix" do
-		
-	end
-
 	describe "#Technique" do
-		
+		it "should contain a widget to credit the playlist creator" do
+			VCR.use_cassette('stream-technique') do
+				visit technique_stream_url
+				page.should have_content "Das Programm auf diesem Kanal wird von"
+			end
+		end
 	end
 
 	describe "#Culture" do
-		
+		it "should contain a widget to credit the playlist creator" do
+			VCR.use_cassette('stream-culture') do
+				visit culture_stream_url
+				page.should have_content "Das Programm auf diesem Kanal wird von"
+			end
+		end
 	end
 	
 end
